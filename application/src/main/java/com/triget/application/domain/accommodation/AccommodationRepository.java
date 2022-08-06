e86e10ebb86dce41249df0eec91c931c9bd200e7
KimHyerin <mye785c83@gmail.com>
2022-08-07 03:23:42 +0900

    :fire: 불필요한 find 함수 제거

--- application/src/main/java/com/triget/application/domain/accommodation/AccommodationRepository.java
+++ application/src/main/java/com/triget/application/domain/accommodation/AccommodationRepository.java
@@ -12,8 +12,6 @@ import java.util.List;
 
 @Repository
 public interface AccommodationRepository extends MongoRepository<Accommodation, ObjectId> {
-    public List<Accommodation> findByPriceBetween(float priceFrom, float priceTo);
-    public Page<Accommodation> findByPriceBetween(float priceFrom, float priceTo, Pageable pageable);
     public List<Accommodation> findAllByCity(String city, Sort orders);
     @Query(value="{$and:[{city: ?0}, {price: {$gte: ?1, $lte: ?2}}]}")
     public Page<Accommodation> findByCityAndPriceBetween(String city, float priceTo, float priceFrom, Pageable pageable);
