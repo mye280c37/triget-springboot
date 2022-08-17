f28a040b6ab60edb6d97dad53adbb04fdf93be19
김혜린 <mye785c83@gmail.com>
2022-08-17 23:05:08 +0900

    :construction: skyscanner api response object 설계중

---
+++ application/src/main/java/com/triget/application/web/dto/skyscanner/Bucket.java
@@ -0,0 +1,9 @@
+package com.triget.application.web.dto.skyscanner;
+
+import java.util.List;
+
+public class Bucket {
+    private String id;
+    private String name;
+    private List<Item> items;
+}
---
+++ application/src/main/java/com/triget/application/web/dto/skyscanner/Context.java
@@ -0,0 +1,7 @@
+package com.triget.application.web.dto.skyscanner;
+
+public class Context {
+    private String status;
+    private String sessionId;
+    private int totalResults;
+}
