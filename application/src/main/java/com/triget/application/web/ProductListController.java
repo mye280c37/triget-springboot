95e884737f15916e2d994ca70d01fb371d73f705
김혜린 <mye785c83@gmail.com>
2022-09-28 13:03:02 +0900

    :zap: reqeust validation 추가, invalid request 처리

--- application/src/main/java/com/triget/application/web/ProductListController.java
+++ application/src/main/java/com/triget/application/web/ProductListController.java
@@ -1,5 +1,6 @@
 package com.triget.application.web;
 
+import com.triget.application.common.ErrorResponse;
 import com.triget.application.service.ProductListServiceImpl;
 import com.triget.application.web.dto.EntireProductListRequestDto;
 import com.triget.application.web.dto.EntireProductListResponseDto;
@@ -9,9 +10,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/product-list/v1")
@@ -28,14 +35,27 @@ public class ProductListController {
     }
 
     @PostMapping()
-    @ApiOperation(value = "여행 스펙 저장, 상품 전체 추천", response = EntireProductListRequestDto.class)
-    public EntireProductListResponseDto returnEntireProductList(@RequestBody EntireProductListRequestDto dto) throws ParseException {
-        ObjectId journeyId = productListServiceImpl.saveRequestDto(dto);
-        return productListServiceImpl.setResponseDto(journeyId);
+    @ApiOperation(value = "여행 스펙 저장, 상품 전체 추천", response = EntireProductListResponseDto.class)
+    public ResponseEntity<?> returnEntireProductList(@Validated @RequestBody EntireProductListRequestDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+            // or 400 bad request
+            return ResponseEntity.badRequest().body(new ErrorResponse("400", "Validation failure", errors));
+        }
+        try {
+            ObjectId journeyId = productListServiceImpl.saveRequestDto(dto);
+            return ResponseEntity.ok(
+                    productListServiceImpl.setResponseDto(journeyId)
+            );
+        }catch (Exception e){
+            // 200 response with 404 status code
+            // errorContent 바꿔야 할듯
+            return ResponseEntity.ok(new ErrorResponse("404", "response failure", e.getMessage()));
+        }
     }
 
     @GetMapping()
-    @ApiOperation(value = "해당 여행 스펙에 대한 상품 전체 추천 리스트", response = EntireProductListRequestDto.class)
+    @ApiOperation(value = "해당 여행 스펙에 대한 상품 전체 추천 리스트", response = EntireProductListResponseDto.class)
     public EntireProductListResponseDto returnEntireProductList(@RequestParam("journeyId") String journeyId) {
         return productListServiceImpl.setResponseDto(new ObjectId(journeyId));
     }
--- application/src/main/java/com/triget/application/web/dto/EntireProductListRequestDto.java
+++ application/src/main/java/com/triget/application/web/dto/EntireProductListRequestDto.java
@@ -1,23 +1,55 @@
 package com.triget.application.web.dto;
 
 import com.triget.application.domain.journey.Journey;
-import com.triget.application.domain.theme.JourneyTheme;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 
-import java.text.ParseException;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
+@ApiModel(value = "여행 스펙 정보", description = "상품 추천을 위해 필요한 사용자의 여행 정보")
 public class EntireProductListRequestDto {
 
+    @NotBlank(message = "place is mandatory")
+    @ApiModelProperty(value = "여행 장소", example = "도쿄")
     private final String place;
+    @NotBlank(message = "theme is mandatory")
+    @ApiModelProperty(value = "여행 테마", example = "relaxing")
     private final String theme;
+    @Min(value = 1, message = "peopleNum must be larger than 0")
+    @ApiModelProperty(value = "여행 총 인원", example = "2")
     private final int peopleNum;
+    @NotBlank(message = "departureDate is mandatory")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Invalid expression. It must follow expression 'yyyy-MM-dd' (ex. 2022-12-04)")
+    @ApiModelProperty(value = "여행 출발 날짜", example = "2022-12-04")
     private final String departureDate;
+    @NotBlank(message = "arrivalDate is mandatory")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Invalid expression. It must follow expression 'yyyy-MM-dd' (ex. 2022-12-08)")
+    @ApiModelProperty(value = "여행 도착 날짜", example = "2022-12-08")
     private final String arrivalDate;
+    @NotBlank(message = "departureAirport is mandatory")
+    @ApiModelProperty(value = "출발 시 이용할 공항 IATA 코드", example = "GMP")
     private final String departureAirport;
+    @ApiModelProperty(value = "모든 인원이 여행에 쓸 총 예산", example = "3000000")
     private final int budget;
+    @Min(value = 1, message = "flightsPrior must have a value between 1 and 5.")
+    @Max(value = 5, message = "flightsPrior must have a value between 1 and 5.")
+    @ApiModelProperty(value = "항공 우선순위", example = "2")
     private final int flightsPrior;
+    @Min(value = 1, message = "accommodationsPrior must have a value between 1 and 5.")
+    @Max(value = 5, message = "accommodationsPrior must have a value between 1 and 5.")
+    @ApiModelProperty(value = "숙박 우선순위", example = "4")
     private final int accommodationsPrior;
+    @Min(value = 1, message = "restaurantsPrior must have a value between 1 and 5.")
+    @Max(value = 5, message = "restaurantsPrior must have a value between 1 and 5.")
+    @ApiModelProperty(value = "식당 우선순위", example = "4")
     private final int restaurantsPrior;
+    @Min(value = 1, message = "attractionsPrior must have a value between 1 and 5.")
+    @Max(value = 5, message = "attractionsPrior must have a value between 1 and 5.")
+    @ApiModelProperty(value = "어트랙션 우선순위", example = "5")
     private final int attractionsPrior;
 
     @Builder
