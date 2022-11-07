10c2363c84aeedd6002f0645c70b80e778959523
김혜린 <mye785c83@gmail.com>
2022-11-07 21:08:05 +0900

    :sparkles: 스케줄 생성 및 저장 기능 완성

---
+++ application/src/main/java/com/triget/application/server/controller/ScheduleController.java
@@ -0,0 +1,31 @@
+package com.triget.application.server.controller;
+
+import com.triget.application.server.controller.dto.LikeProductList;
+import com.triget.application.server.domain.schedule.Schedule;
+import com.triget.application.server.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/schedule")
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    @Autowired
+    private final ScheduleService scheduleService;
+
+    @PostMapping()
+    public Schedule createSchedule(@Validated @RequestBody LikeProductList likeProductList) {
+        return scheduleService.createSchedule(likeProductList);
+    }
+
+    @PostMapping("/auth")
+    public Schedule saveSchedule(@Validated @RequestBody Schedule schedule) {
+        return scheduleService.saveSchedule(schedule);
+    }
+}
